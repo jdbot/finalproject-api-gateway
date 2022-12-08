167e296c4b51c799f7dacf1d6b6319f7a8267554
Jorge David Prado Coloma <jpradoco@emeal.nttdata.com>
2022-12-07 22:21:54 -0500

    Subiendo api-gateway

---
+++ src/main/java/com/nttdata/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.nttdata.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class.
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
